be4372c733f63f451e20cfbac387edd2bedceec6
inrage <70843287+inrag3@users.noreply.github.com>
2023-02-09 23:22:04 +0300

    Add templates and includes

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,7 +1,10 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    return render(request, template)
+
 
 def group_posts(request, slug):
-    return HttpResponse(f'Slug строка: {slug}')
+    template = 'posts/group_list.html'
+    return render(request, template)
