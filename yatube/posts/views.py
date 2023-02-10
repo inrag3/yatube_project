940fcce0d33a676c4578196d6ee24b89ab9dfdca
inrage <70843287+inrag3@users.noreply.github.com>
2023-02-10 19:17:03 +0300

    Add render group posts

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post, Group
 
 def index(request):
     template = 'posts/index.html'
@@ -11,8 +13,11 @@ def index(request):
 
 def group_posts(request, slug):
     template = 'posts/group_list.html'
-    text = 'Здесь будет информация о группах проекта Yatube'
+    group = get_object_or_404(Group, slug=slug)
+
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'text': text,
+        'group': group,
+        'posts': posts,
     }
     return render(request, template, context)
